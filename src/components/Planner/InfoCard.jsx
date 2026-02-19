244e4189e01a8d5b7882d033b805eebc323244d8
Pariskrit12 <bhagataryan784@gmail.com>
2026-02-19 23:46:17 +0545

    feat: add reusable infoCard component

---
+++ src/components/Planner/InfoCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const InfoCard = ({ value = "--", label, textColor = "text-purple-500" }) => {
+  return (
+    <div className="p-2 w-30 rounded-xl  flex flex-col items-center shadow-[0_0_5px_rgba(0,0,0,0.25)]  ">
+      <div className={`text-xl font-bold ${textColor}`}>{value}</div>
+      <div className="text-sm text-gray-500 text-center mt-1">{label}</div>
+    </div>
+  );
+};
+
+export default InfoCard;
