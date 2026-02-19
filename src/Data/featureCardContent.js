27c905f62d917e6da20155d0df7feabaf4adc4e4
Pariskrit12 <bhagataryan784@gmail.com>
2026-02-20 00:33:12 +0545

    refactor: add data folder for reusable static data

---
+++ src/Data/featureCardContent.js
@@ -0,0 +1,23 @@
+ export const featureCardContent = [
+    {
+      featureName: "Fast",
+      description: "Instant route optimization",
+      bg_color: "dbeafe",
+      iconColor: "blue",
+      icon: "basil:lightning-alt-outline",
+    },
+    {
+      featureName: "Save",
+      description: "Reduce fuel costs",
+      bg_color: "dbfce7",
+      iconColor: "green",
+      icon: "mingcute:currency-dollar-line",
+    },
+    {
+      featureName: "Efficient",
+      description: "Minimize travel time",
+      bg_color: "f3e8ff",
+      iconColor: "purple",
+      icon: "uis:clock-eight",
+    },
+  ];
