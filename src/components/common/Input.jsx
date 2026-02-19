540812c8a2fc56dcfd4217793c3332cbc1c1bdea
Pariskrit12 <bhagataryan784@gmail.com>
2026-02-19 22:25:36 +0545

    feat: added value and onChange fetaure in input components

--- src/components/common/Input.jsx
+++ src/components/common/Input.jsx
@@ -1,16 +1,18 @@
 import { Icon } from "@iconify/react";
 import React from "react";
 
-const Input = ({label,icon,placeholder}) => {
+const Input = ({label,icon,placeholder,value,onChange}) => {    
   return (
     <div className="flex flex-col gap-2">
       <label className="font-bold">{label}</label>
       <div className="flex items-center gap-2  p-3 rounded-xl bg-gray-200">
-        <Icon icon={icon} width={30} height={30} />
+        <Icon icon={icon} width={20} height={20} />
         <input
           type="text"
           placeholder={placeholder}
           className="outline-none"
+          value={value}
+          onChange={onChange}
         />
       </div>
     </div>
