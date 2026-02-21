5dd22b76b90f7171c836d7cbe2dc32f9cb6dda02
Pariskrit12 <bhagataryan784@gmail.com>
2026-02-21 17:08:10 +0545

    feat: add resultInfoCardData config for rendering ResultInfoCard dynamically

---
+++ src/Data/ResultData/resultInfoCardData.js
@@ -0,0 +1,23 @@
+export const resultInfoCardData = [
+  {
+    color:"#3b82f6",
+    icon: "lsicon:path-outline",
+    value: "239 mi",
+    label: "Total Distance",
+    tag: "Total",
+  },
+  {
+    color: "#22c55e",
+    icon: "mynaui:dollar-solid",
+    value: "$33.46",
+    label: "Total Fuel Cost",
+    tag: "Est.",
+  },
+  {
+    color: "#a855f7",
+    icon: "iconamoon:clock-thin",
+    value: "4h 36m",
+    label: "Estimated Time",
+    tag: "Est.",
+  },
+];
