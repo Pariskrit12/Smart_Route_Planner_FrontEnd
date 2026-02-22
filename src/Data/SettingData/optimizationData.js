af462dc31cdfc4ce840e25408dc07599f6228ec9
Pariskrit12 <bhagataryan784@gmail.com>
2026-02-23 00:17:01 +0545

    Added optimizationData array for route optimization options with labels and icons

---
+++ src/Data/SettingData/optimizationData.js
@@ -0,0 +1,15 @@
+export const optimizationData = [
+  {
+    label: "Fastest Route-Minimize travel time",
+    icon: "basil:lightning-alt-outline",
+  },
+  { label: "Cheapest Route-Minimize fuel cost", icon: "iconoir:dollar" },
+  {
+    label: "Balanced-Best time and cost trade-off",
+    icon: "pepicons-pencil:paper-plane",
+  },
+  {
+    label: "Shortest Distance-Minimize miles travelled",
+    icon: "pepicons-pencil:map",
+  },
+];
