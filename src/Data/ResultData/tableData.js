4b6515269a44cfa737ebbaa448c21b73432c40fb
Pariskrit12 <bhagataryan784@gmail.com>
2026-02-22 17:56:41 +0545

    feat: add mock tableData for route stops

---
+++ src/Data/ResultData/tableData.js
@@ -0,0 +1,7 @@
+export const tableData=[
+    {location:"Biratnagar"},
+    { location: "Kathmandu", distance: "45 mi", price: "$45 " },
+    { location: "Pokahara", distance: "67 mi", price: "$47 " },
+    { location: "Lumbini", distance: "78 mi", price: "$55 " },
+    { location: "Dhangadhi", distance: "120 mi", price: "$90 " },
+]
