bb6592e2fa974e0aa31e70b869fee9100caf65aa
Pariskrit12 <bhagataryan784@gmail.com>
2026-02-24 16:04:39 +0545

    feat: add onClick support to Button component

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,9 +1,9 @@
 import { Icon } from '@iconify/react'
 import React from 'react'
 
-const Button = ({className,name,icon}) => {
+const Button = ({className,name,icon,onClick}) => {
   return (
-  <div className={className}>
+  <div onClick={onClick} className={className}>
 
     <button>{name}</button>
     <Icon icon={icon} width={20} height={20}/>
